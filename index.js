4c397477cfbae650366ff667a756e9e38ee989e8
apurvashrivastava01 <131666531+apurvashrivastava01@users.noreply.github.com>
2023-04-25 21:23:34 +0530

    updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 require("./config/db")
 const cors = require('cors');
 const path=require('path');
-const (fileURLToPath)=require('url');
+const {fileURLToPath}=require('url');
 // mongoose.connect('mongodb://localhost:27017/mern-ecommerce');
 
 const app = express();
